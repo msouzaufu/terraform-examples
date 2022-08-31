4c26db359539dcbae0a028e5f9ae50b528b51ae6
Matheus Souza <matheus-rodrigues-00@hotmail.com>
2022-08-30 21:52:32 -0300

    update filter architecture

--- 02-terraform-intermediario/03-data-source/data.tf
+++ 02-terraform-intermediario/03-data-source/data.tf
@@ -1,6 +1,11 @@
 data "aws_ami" "ubuntu" {
   owners = ["amazon"]
   most_recent = true
-  name_regex = "ubuntu"
+  name_regex = "Ubuntu"
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
 
 }
